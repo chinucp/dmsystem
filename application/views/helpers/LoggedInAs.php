0ccb006602e5aba99c464ad945fb4768556b6c96
Arun Nampoothiri <arun.vishnu.n@gmail.com>
2012-04-01 15:09:06 +0530

    Dashboard view changes (CSS)

--- application/views/helpers/LoggedInAs.php
+++ application/views/helpers/LoggedInAs.php
@@ -1,23 +1,28 @@
 <?php
-class Zend_View_Helper_LoggedInAs extends Zend_View_Helper_Abstract
-{
-    public function loggedInAs ()
-    {
+
+class Zend_View_Helper_LoggedInAs extends Zend_View_Helper_Abstract {
+
+    public function loggedInAs() {
         $auth = Zend_Auth::getInstance();
         if ($auth->hasIdentity()) {
             $username = $auth->getIdentity()->username;
-            $logoutUrl = $this->view->url(array('controller'=>'index',
-                'action'=>'logout'), null, true);
-            return 'Signed in as: <b>' . ucfirst($username) .  '</b>. Click here to<a href="'.$logoutUrl.'">Sign out</a>';
+            $firstName = $auth->getIdentity()->first_name;
+            $lastName = $auth->getIdentity()->last_name;
+            $logoutUrl = $this->view->url(array(
+                'controller' => 'index', 'action' => 'logout'
+            ), null, true);
+            return 'Signed in as: <b>' . ucfirst($firstName) . ' ' . $lastName . '</b>. Click here to<a href="'
+                            . $logoutUrl . '">Sign out</a>';
         }
-
         $request = Zend_Controller_Front::getInstance()->getRequest();
         $controller = $request->getControllerName();
         $action = $request->getActionName();
-        if($controller == 'index' && $action == 'index') {
+        if ($controller == 'index' && $action == 'index') {
             return '';
         }
-        $loginUrl = $this->view->url(array('controller'=>'index', 'action'=>'index'));
-        return '<a href="'.$loginUrl.'">Login</a>';
+        $loginUrl = $this->view->url(array(
+            'controller' => 'index', 'action' => 'index'
+        ));
+        return '<a href="' . $loginUrl . '">Login</a>';
     }
 }
