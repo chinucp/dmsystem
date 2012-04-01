f48bd21444cf84d51350a95d575dc854b3c43322
Arun Nampoothiri <arun.vishnu.n@gmail.com>
2012-04-01 15:10:38 +0530

    Changes in showing signed in as

--- application/views/helpers/LoggedInAs.php
+++ application/views/helpers/LoggedInAs.php
@@ -11,7 +11,7 @@ class Zend_View_Helper_LoggedInAs extends Zend_View_Helper_Abstract {
             $logoutUrl = $this->view->url(array(
                 'controller' => 'index', 'action' => 'logout'
             ), null, true);
-            return 'Signed in as: <b>' . ucfirst($firstName) . ' ' . $lastName . '</b>. Click here to<a href="'
+            return 'Signed in as: <b>' . ucfirst($firstName) . ' ' . ucfirst($lastName) . '</b>. Click here to<a href="'
                             . $logoutUrl . '">Sign out</a>';
         }
         $request = Zend_Controller_Front::getInstance()->getRequest();
