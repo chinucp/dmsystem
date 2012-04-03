85bf60fcdadb32b886cf4fa229f775cdfa97bdef
chinu cp <chinu.cp@gmail.com>
2012-04-03 20:20:51 +0530

    updated basecontroller

--- application/controllers/BaseController.php
+++ application/controllers/BaseController.php
@@ -3,23 +3,49 @@
 class BaseController extends Zend_Controller_Action {
 
     public function init() {
-    	$this->authentication();
+    	// selection of navigation menu
+    	$this->navigationSelection();
+
+    	// check for authectication for each controller call
+		$this->authentication();
+
     }
 
     public function indexAction() {
 
     }
-    public function authentication(){
+    /**
+     * @desc highlighting the navigation menu
+     * @param void
+     * @return void
+     */
+    protected function navigationSelection(){
+       	$uri = $this->_request->getPathInfo();
+        $baseUrl = Zend_Controller_Front::getInstance()->getBaseUrl();
+        $activeNav = $this->view->navigation()->findByUri($baseUrl . $uri);
+        $activeNav->active = true;
+    }
+    /**
+     *
+     * @desc check for authenticated user
+     * @param void
+     * @return void
+     *
+     */
+    protected function authentication(){
     	$auth = Zend_Auth::getInstance();
     	if ($auth->hasIdentity()) {
 			$authData = $auth->getIdentity();
+			$this->setRole($authData->user_types_id);
 		}else{
 			// If authentication failed then go to the login page
 			$this->_redirect('/');
 		}
     }
-    public function fetchRole(){
-       //TODO fetch role name
+
+    public function setRole($id){
+		$role = new Zend_Session_Namespace("role");
+		$role->name = 'level'.$id;
     }
 
 }
