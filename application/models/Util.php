ecedbc3cfbfc8d2b9e0b132f84c9de4a379a5274
chinu cp <chinu.cp@gmail.com>
2012-04-07 00:27:03 +0530

    graph code

---
+++ application/models/Util.php
@@ -0,0 +1,19 @@
+<?php
+
+class Application_Model_Util
+{
+	
+	public function __construct(array $options = null)
+	{
+		
+	}
+
+	public function graphFileName($suffix=''){
+		$authSession = new Application_Model_Auth();
+		$user = $authSession->getAuthName();
+		
+		return $user.'_'.$suffix;
+	}
+	
+}
+
